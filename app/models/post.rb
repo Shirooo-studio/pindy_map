f40568fb6841bf614e505e3b99254dd3fe6b6c9f
片岡誠之郎 <kataokashigeyukirou@kataokashigeyukirounoMacBook-Air-2.local>
2025-09-20 14:33:14 +0900

    add_posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
 class Post < ApplicationRecord
+  belongs_to :user, optional: false
+  belongs_to :pin, optional: true
+
+  enum visibility: { everyone: 0, company_only: 1, private_: 2 }, _prefix: :visibility
+
+  has_many_attached :media
+  validate :media_count_limit
+
+  if defined?(ActiveStorageValidations)
+    validates :media,
+    content_type: [ /\Aimage\/.*\z/, /\Avideo\/.*\z/ ],
+    size: { less_than: 200.megabytes }
+  end
+
+  private
+  def media_count_limit
+    count =
+      if media.attachments.loaded?
+        media.attachments.size
+      else
+        media.count
+      end
+    if count > 10
+      errors.add(:media, "は合計10個までアップロードできます")
+    end
+  end
 end
