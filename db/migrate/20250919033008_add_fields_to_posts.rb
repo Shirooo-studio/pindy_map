3aca989c927bba47ef95fd4beb617f264215c459
片岡誠之郎 <kataokashigeyukirou@kataokashigeyukirounoMacBook-Air-2.local>
2025-09-26 14:16:35 +0900

    emprove error

--- db/migrate/20250919033008_add_fields_to_posts.rb
+++ db/migrate/20250919033008_add_fields_to_posts.rb
@@ -12,13 +12,13 @@ class AddFieldsToPosts < ActiveRecord::Migration[7.2]
     end
 
     # 取得最適化（既存なら作らない）
-    add_index :posts, [:pin_id, :created_at] unless index_exists?(:posts, [:pin_id, :created_at])
+    add_index :posts, [ :pin_id, :created_at ] unless index_exists?(:posts, [ :pin_id, :created_at ])
     add_index :posts, :user_id                unless index_exists?(:posts, :user_id)
   end
 
   def down
     # ロールバック時のみ実行される想定。存在時だけ削除
-    remove_index  :posts, column: [:pin_id, :created_at] if index_exists?(:posts, [:pin_id, :created_at])
+    remove_index  :posts, column: [ :pin_id, :created_at ] if index_exists?(:posts, [ :pin_id, :created_at ])
     remove_index  :posts, column: :user_id                if index_exists?(:posts, :user_id)
 
     remove_column :posts, :visibility if column_exists?(:posts, :visibility)
